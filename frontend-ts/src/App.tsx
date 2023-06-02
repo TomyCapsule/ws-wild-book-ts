4f9d238578ea660535e027fd30e8fea8b8cb84a2
Tomy Visouthiphongs <tvisouthiphong@gmail.com>
2023-06-02 10:19:12 +0200

    Removed unused provider

--- frontend-ts/src/App.tsx
+++ frontend-ts/src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import { WildersListProvider } from "./context/WildersList";
 import MainContent from "./pages";
 
 
@@ -10,9 +9,7 @@ function App() {
   return (
       <div>
         <Header />
-        <WildersListProvider>
           <MainContent />
-        </WildersListProvider>
         <Footer />
       </div>
   );
